dcc7b7828d8802627f98fb19cfdcf6f4aba66958
faisalmoinuddin99 <faisal25march99@gmail.com>
2023-03-25 13:34:39 +0530

    NESTED ELEMENT

---
+++ App.js
@@ -0,0 +1,26 @@
+/*
+ *
+ *
+ * <div id="parent">
+ *      <div id="child">
+ *          <h1> I'm an h1 tag </h1>
+ *      </div>
+ * </div>
+ *
+ */
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "I'm an h1 tag")
+  )
+);
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
