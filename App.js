1ae31a8ce061aa9642a7490054b6c74602a63236
faisalmoinuddin99 <faisal25march99@gmail.com>
2023-04-19 10:53:52 +0530

    using map to display all restaurant present in array instead of hardcoding

--- App.js
+++ App.js
@@ -1777,14 +1777,9 @@ const Body = () => {
       <div className="search">Search</div>
       <div className="restaurant-container">
         {/* RestaurantCard */}
-        <RestaurantCard resData={resList[0]} />
-        <RestaurantCard resData={resList[1]} />
-        <RestaurantCard resData={resList[2]} />
-        <RestaurantCard resData={resList[3]} />
-        <RestaurantCard resData={resList[4]} />
-        <RestaurantCard resData={resList[5]} />
-        <RestaurantCard resData={resList[6]} />
-        <RestaurantCard resData={resList[7]} />
+        {resList.map((restaurant) => (
+          <RestaurantCard resData={restaurant} />
+        ))}
       </div>
     </div>
   );
